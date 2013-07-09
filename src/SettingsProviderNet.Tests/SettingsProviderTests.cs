22545912fc28882709fa9d095733aef75faef8b7
Jake Ginnivan <jake.ginnivan@readify.net>
2013-07-09 12:07:18 +0800

    Fixed a large issue and changed tests so they picked the issue up

--- src/SettingsProviderNet.Tests/SettingsProviderTests.cs
+++ src/SettingsProviderNet.Tests/SettingsProviderTests.cs
@@ -4,100 +4,24 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Globalization;
 using System.Linq;
-using NSubstitute;
 using Xunit;
 
 namespace SettingsProviderNet.Tests
 {
     public class SettingsProviderTests
     {
-        readonly Dictionary<string, string> backingStore;
         readonly SettingsProvider settingsRetreiver;
         readonly SettingsProvider settingsSaver;
 
         public SettingsProviderTests()
         {
-            var applicationSettingsStore = Substitute.For<ISettingsStorage>();
-            var store = new IsolatedStorageSettingsStore();
-            applicationSettingsStore.SerializeList(Arg.Any<List<string>>())
-                .Returns(c => store.SerializeList(c.Arg<List<string>>()));
-            applicationSettingsStore.DeserializeList(Arg.Any<string>())
-                .Returns(c => store.DeserializeList(c.Arg<string>()));
-            backingStore = new Dictionary<string, string>();
-            applicationSettingsStore.Load("TestSettings").Returns(backingStore);
-            applicationSettingsStore
-                .When(s=>s.Save("TestSettings", Arg.Any<Dictionary<string, string>>()))
-                .Do(c=>
-                    {
-                        backingStore.Clear();
-                        foreach (var setting in c.Arg<Dictionary<string, string>>())
-                        {
-                            backingStore.Add(setting.Key, setting.Value);
-                        }
-                    });
-
-            settingsRetreiver = new SettingsProvider(applicationSettingsStore);
-            settingsSaver = new SettingsProvider(applicationSettingsStore);
+            var store = new TestStorage();
+            settingsRetreiver = new SettingsProvider(store);
+            settingsSaver = new SettingsProvider(store);
         }
 
         [Fact]
-        public void settings_provider_can_persist_int()
-        {
-            // arrange
-            var settings = new TestSettings { TestProp2 = 123 };
-            var settingDescriptor = new SettingsProvider.SettingDescriptor(typeof(TestSettings).GetProperty("TestProp2"));
-            var key = SettingsProvider.GetKey<TestSettings>(settingDescriptor);
-
-            // act
-            settingsSaver.SaveSettings(settings);
-
-            // assert
-            Assert.Equal(settings.TestProp2, Convert.ToInt32(backingStore[key]));
-        }
-
-        [Fact]
-        public void settings_provider_can_persist_list()
-        {
-            // arrange
-            var settings = new TestSettings { List = new List<string>{"Testing", "Testing2"} };
-            var settingDescriptor = new SettingsProvider.SettingDescriptor(typeof(TestSettings).GetProperty("List"));
-            var key = SettingsProvider.GetKey<TestSettings>(settingDescriptor);
-
-            // act
-            settingsSaver.SaveSettings(settings);
-
-            // assert
-            Assert.Equal("[\"Testing\",\"Testing2\"]", backingStore[key]);
-        }
-
-        [Fact]
-        public void settings_provider_can_retreive_list()
-        {
-            // arrange
-            settingsSaver.SaveSettings(new TestSettings { List2 = new List<int>{123} });
-
-            // act
-            var settings = settingsRetreiver.GetSettings<TestSettings>();
-
-            // assert
-            Assert.Equal(123, settings.List2.Single());
-        }
-
-        [Fact]
-        public void settings_provider_can_retreive_list_with_japanese_characters()
-        {
-            // arrange
-            settingsSaver.SaveSettings(new TestSettings { List = new List<string> { "居酒屋" } });
-
-            // act
-            var settings = settingsRetreiver.GetSettings<TestSettings>();
-
-            // assert
-            Assert.Equal("居酒屋", settings.List.Single());
-        }
-
-        [Fact]
-        public void settings_provider_can_retreive_int()
+        public void settings_provider_can_save_and_persist_int()
         {
             // arrange
             settingsSaver.SaveSettings(new TestSettings { TestProp2 = 123 });
@@ -110,37 +34,7 @@ namespace SettingsProviderNet.Tests
         }
 
         [Fact]
-        public void settings_provider_can_persist_string()
-        {
-            // arrange
-            var settings = new TestSettings { TestProp1 = "bar" };
-            var settingDescriptor = new SettingsProvider.SettingDescriptor(typeof(TestSettings).GetProperty("TestProp1"));
-            var key = SettingsProvider.GetKey<TestSettings>(settingDescriptor);
-
-            // act
-            settingsSaver.SaveSettings(settings);
-
-            // assert
-            Assert.Equal(settings.TestProp1, backingStore[key]);
-        }
-
-        [Fact]
-        public void settings_provider_can_persist_japanese_string()
-        {
-            // arrange
-            var settings = new TestSettings { TestProp1 = "居酒屋" };
-            var settingDescriptor = new SettingsProvider.SettingDescriptor(typeof(TestSettings).GetProperty("TestProp1"));
-            var key = SettingsProvider.GetKey<TestSettings>(settingDescriptor);
-
-            // act
-            settingsSaver.SaveSettings(settings);
-
-            // assert
-            Assert.Equal(settings.TestProp1, backingStore[key]);
-        }
-
-        [Fact]
-        public void settings_provider_can_retreive_string()
+        public void settings_provider_can_save_and_retreive_string()
         {
             // arrange
             settingsSaver.SaveSettings(new TestSettings { TestProp1 = "bar" });
@@ -153,23 +47,20 @@ namespace SettingsProviderNet.Tests
         }
 
         [Fact]
-        public void settings_provider_can_persist_nullable_datetime()
+        public void settings_provider_can_save_and_retreive_japanese_string()
         {
             // arrange
-            var firstRun = DateTime.Now;
-            var settings = new TestSettings { FirstRun = firstRun };
-            var settingDescriptor = new SettingsProvider.SettingDescriptor(typeof(TestSettings).GetProperty("FirstRun"));
-            var key = SettingsProvider.GetKey<TestSettings>(settingDescriptor);
+            settingsSaver.SaveSettings(new TestSettings { TestProp1 = "居酒屋" });
 
             // act
-            settingsSaver.SaveSettings(settings);
+            var settings = settingsRetreiver.GetSettings<TestSettings>();
 
             // assert
-            Assert.Equal(firstRun.ToString(CultureInfo.InvariantCulture), backingStore[key]);
+            Assert.Equal("居酒屋", settings.TestProp1);
         }
 
         [Fact]
-        public void settings_provider_can_retreive_nullable_datetime()
+        public void settings_provider_can_save_and_retreive_nullable_datetime()
         {
             // arrange
             var firstRun = DateTime.Now;
@@ -185,7 +76,7 @@ namespace SettingsProviderNet.Tests
         }
 
         [Fact]
-        public void settings_provider_can_retreive_null_nullable_datetime()
+        public void settings_provider_can_save_and_retreive_null_nullable_datetime()
         {
             // arrange
             settingsSaver.SaveSettings(new TestSettings { FirstRun = null });
@@ -195,6 +86,32 @@ namespace SettingsProviderNet.Tests
 
             // assert
             Assert.Null(settings.FirstRun);
+        }
+
+        [Fact]
+        public void settings_provider_can_save_and_retreive_list()
+        {
+            // arrange
+            settingsSaver.SaveSettings(new TestSettings { List2 = new List<int>{123} });
+
+            // act
+            var settings = settingsRetreiver.GetSettings<TestSettings>();
+
+            // assert
+            Assert.Equal(123, settings.List2.Single());
+        }
+
+        [Fact]
+        public void settings_provider_can_save_and_retreive_list_with_japanese_characters()
+        {
+            // arrange
+            settingsSaver.SaveSettings(new TestSettings { List = new List<string> { "居酒屋" } });
+
+            // act
+            var settings = settingsRetreiver.GetSettings<TestSettings>();
+
+            // assert
+            Assert.Equal("居酒屋", settings.List.Single());
         }
 
         [Fact]
@@ -222,22 +139,7 @@ namespace SettingsProviderNet.Tests
         }
 
         [Fact]
-        public void settings_provider_can_persist_enum()
-        {
-            // arrange
-            var settings = new TestSettings { SomeEnum = MyEnum.Value1 };
-            var settingDescriptor = new SettingsProvider.SettingDescriptor(typeof(TestSettings).GetProperty("SomeEnum"));
-            var key = SettingsProvider.GetKey<TestSettings>(settingDescriptor);
-
-            // act
-            settingsSaver.SaveSettings(settings);
-
-            // assert
-            Assert.Equal(Enum.GetName(typeof(MyEnum), settings.SomeEnum), backingStore[key]);
-        }
-
-        [Fact]
-        public void settings_provider_can_retreive_enum()
+        public void settings_provider_can_save_and_retreive_enum()
         {
             // arrange
             settingsSaver.SaveSettings(new TestSettings { SomeEnum = MyEnum.Value2 });
@@ -342,6 +244,29 @@ namespace SettingsProviderNet.Tests
             public class ComplexType
             {
             }
+        }
+    }
+
+    public class TestStorage : JsonSettingsStoreBase
+    {
+        private readonly Dictionary<string, string> files = new Dictionary<string, string>();
+
+        public Dictionary<string, string> Files
+        {
+            get { return files; }
+        }
+
+        protected override void WriteTextFile(string filename, string fileContents)
+        {
+            if (!Files.ContainsKey(filename))
+                Files.Add(filename, fileContents);
+            else
+                Files[filename] = fileContents;
+        }
+
+        protected override string ReadTextFile(string filename)
+        {
+            return Files.ContainsKey(filename) ? Files[filename] : null;
         }
     }
 
