6bf4fc2f8ca1cd085971db09fa38f5abc50b10f4
Jake Ginnivan <jake.ginnivan@readify.net>
2014-01-02 20:07:23 +0000

    Set up GitHubFlowVersion

--- Build.cmd
+++ Build.cmd
@@ -1,8 +1,6 @@
 @ECHO OFF
 
-SET msbuild="%windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe"
-
-%msbuild% SettingsProvider.proj
+tools\GitHubFlowVersion\GitHubFlowVersion.exe /p SettingsProvider.proj /UpdateAssemblyInfo
 
 IF NOT ERRORLEVEL 0 EXIT /B %ERRORLEVEL%
 
