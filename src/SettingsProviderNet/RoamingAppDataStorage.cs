f82ed43053faf80968ccd0ef14d0a87bf611155e
Jake Ginnivan <jake.ginnivan@readify.net>
2014-01-03 10:38:58 +0000

    Minor fixes in new roaming app data storage

--- src/SettingsProviderNet/RoamingAppDataStorage.cs
+++ src/SettingsProviderNet/RoamingAppDataStorage.cs
@@ -14,18 +14,22 @@ namespace SettingsProviderNet
 
         protected override void WriteTextFile(string filename, string fileContents)
         {
-            File.WriteAllText(GetSettingsFilename(filename), fileContents);
+            var settingsFolder = GetSettingsFolder();
+            if (!Directory.Exists(settingsFolder))
+                Directory.CreateDirectory(settingsFolder);
+            File.WriteAllText(Path.Combine(settingsFolder, filename), fileContents);
         }
 
         protected override string ReadTextFile(string filename)
         {
-            return File.ReadAllText(GetSettingsFilename(filename));
+            var settingsFilename = Path.Combine(GetSettingsFolder(), filename);
+            return File.Exists(settingsFilename) ? File.ReadAllText(settingsFilename) : null;
         }
 
-        private string GetSettingsFilename(string filename)
+        private string GetSettingsFolder()
         {
             var folderPath = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData);
-            var settingsFilename = Path.Combine(folderPath, folderName, filename);
+            var settingsFilename = Path.Combine(folderPath, folderName);
             return settingsFilename;
         }
     }
