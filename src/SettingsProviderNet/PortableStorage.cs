feaefe58411235dc475fd94d6f96fba7a7abe246
Xelio Cheong <xelio.cheong@gmail.com>
2014-08-06 14:08:48 +0800

    Add PortableStorage which store the setting file in same folder as the executable

---
+++ src/SettingsProviderNet/PortableStorage.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Reflection;
+
+namespace SettingsProviderNet
+{
+    public class PortableStorage : JsonSettingsStoreBase
+    {
+        public PortableStorage()
+        {
+        }
+
+        protected override void WriteTextFile(string filename, string fileContents)
+        {
+            var settingsFilename = Path.Combine(GetProgramFolder(), filename);
+            File.WriteAllText(settingsFilename, fileContents);
+        }
+
+        protected override string ReadTextFile(string filename)
+        {
+            var settingsFilename = Path.Combine(GetProgramFolder(), filename);
+            return File.Exists(settingsFilename) ? File.ReadAllText(settingsFilename) : null;
+        }
+
+        private string GetProgramFolder()
+        {
+            return Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+        }
+    }
+}
